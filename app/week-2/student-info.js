7b4221727e68d6485157bbd60d29e20b53717136
Camilo Gomez <CamiloAndres.GomezRamirez@edu.sait.ca>
2024-09-24 15:51:24 -0600

    Root page
    Changed root page.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <h1>Camilo Gomez</h1>
+            <Link href="https://github.com/Am00nshapedp00l/cprg306-assignments.git">https:/github.com</Link>
+        </div>
+    );
+};
+
+export default StudentInfo;
